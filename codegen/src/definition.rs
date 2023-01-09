639b9dd24871ee96050fad41d9623c3540023529
ivan770 <ivan@ivan770.me>
2023-01-09 14:39:14 +0200

    Fix incorrect method dispatch, fix tests

--- codegen/src/definition.rs
+++ codegen/src/definition.rs
@@ -3,19 +3,26 @@ use proc_macro2::TokenStream;
 use quote::quote;
 use syn::{
     parse2,
+    parse_quote,
+    Attribute,
     Error,
+    FnArg,
     ItemTrait,
     Lit,
     Meta,
     NestedMeta,
+    ReturnType,
     TraitItem,
-    TraitItemMethod, FnArg, ReturnType, Attribute, parse_quote,
+    TraitItemMethod,
 };
 
 use crate::{
     format_err_spanned,
     types::AttributeArgs,
-    utils::{into_u16, into_u32},
+    utils::{
+        into_u16,
+        into_u32,
+    },
 };
 
 struct TraitAttrs {
@@ -74,14 +81,13 @@ impl Method {
 
         let hash = into_u32(&method_item.sig.ident);
 
-        let input_tys = method_item.sig
-            .inputs
-            .iter()
-            .filter_map(|input| if let FnArg::Typed(pat) = input {
+        let input_tys = method_item.sig.inputs.iter().filter_map(|input| {
+            if let FnArg::Typed(pat) = input {
                 Some(&*pat.ty)
             } else {
                 None
-            });
+            }
+        });
 
         let output_tokens = if let ReturnType::Type(_, ty) = &method_item.sig.output {
             quote!(#ty)
@@ -99,8 +105,13 @@ impl Method {
         })
     }
 
-    fn fill_with_ink_data(&self, method_item: &mut TraitItemMethod) {
-        let Method { id, input_tokens, output_tokens, .. } = self;
+    fn fill_with_ink_data(&self, trait_attrs: &TraitAttrs, method_item: &mut TraitItemMethod) {
+        let Method {
+            id,
+            input_tokens,
+            output_tokens,
+            ..
+        } = self;
 
         let input_bound = parse_quote! {
             #input_tokens: ::scale::Encode
@@ -119,17 +130,20 @@ impl Method {
             });
         }
 
-        let input_bindings = method_item.sig
-            .inputs
-            .iter()
-            .filter_map(|input| if let FnArg::Typed(pat) = input {
+        let input_bindings = method_item.sig.inputs.iter().filter_map(|input| {
+            if let FnArg::Typed(pat) = input {
                 Some(&*pat.pat)
             } else {
                 None
-            });
+            }
+        });
+
+        // https://paritytech.github.io/substrate/master/pallet_contracts/chain_extension/trait.RegisteredChainExtension.html
+        let trait_id = (trait_attrs.id as u32) << 16;
+        let id_for_call = trait_id | (*id as u32);
 
         method_item.default = Some(parse_quote! {{
-            ::obce::ink_lang::env::chain_extension::ChainExtensionMethod::build(#id)
+            ::obce::ink_lang::env::chain_extension::ChainExtensionMethod::build(#id_for_call)
                 .input::<#input_tokens>()
                 .output::<#output_tokens>()
                 .ignore_error_code()
@@ -140,7 +154,7 @@ impl Method {
 
 pub fn generate(attrs: TokenStream, input: TokenStream) -> Result<TokenStream, Error> {
     let mut trait_item: ItemTrait = parse2(input)?;
-    
+
     let trait_attrs = TraitAttrs::new(&trait_item, parse2(attrs)?)?;
 
     let trait_id = trait_attrs.id;
@@ -163,27 +177,37 @@ pub fn generate(attrs: TokenStream, input: TokenStream) -> Result<TokenStream, E
         })
         .try_collect()?;
 
-    let method_descriptions = methods
-        .iter()
-        .map(|Method { id, hash, input_tokens, output_tokens, .. }| quote! {
-            impl #impls ::obce::codegen::MethodDescription<#hash> for dyn #trait_name #types #where_clause {
-                const ID: ::core::primitive::u16 = #id;
-                type Input = #input_tokens;
-                type Output = #output_tokens;
+    let method_descriptions = methods.iter().map(
+        |Method {
+             id,
+             hash,
+             input_tokens,
+             output_tokens,
+             ..
+         }| {
+            quote! {
+                impl #impls ::obce::codegen::MethodDescription<#hash> for dyn #trait_name #types #where_clause {
+                    const ID: ::core::primitive::u16 = #id;
+                    type Input = #input_tokens;
+                    type Output = #output_tokens;
+                }
             }
-        });
+        },
+    );
 
     let mut ink_trait_item = trait_item.clone();
-        
+
     ink_trait_item
         .items
         .iter_mut()
         .zip(methods.iter())
-        .for_each(|(item, method)| if let TraitItem::Method(method_item) = item {
-            method.fill_with_ink_data(method_item);
-        } else {
-            // FIXME: Remove unreachable call by using types?
-            unreachable!("only methods are present here")
+        .for_each(|(item, method)| {
+            if let TraitItem::Method(method_item) = item {
+                method.fill_with_ink_data(&trait_attrs, method_item);
+            } else {
+                // FIXME: Remove unreachable call by using types?
+                unreachable!("only methods are present here")
+            }
         });
 
     Ok(quote! {
