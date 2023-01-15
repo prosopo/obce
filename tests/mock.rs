510118e82487f7b869dabb06b869362f11453ccd
ivan770 <ivan@ivan770.me>
2023-01-15 14:18:34 +0200

    More unit tests

--- tests/mock.rs
+++ tests/mock.rs
@@ -1,6 +1,7 @@
 #[obce::definition]
 pub trait Trait {
     fn method(&mut self, val: u32, another_val: u32) -> u32;
+    fn another_method(&mut self, val: u32) -> u32;
 }
 
 struct TestExtension;
@@ -26,6 +27,11 @@ mod simple_contract {
         #[ink(message)]
         pub fn call_method(&mut self, val: u32, another_val: u32) -> u32 {
             TestExtension.method(val, another_val)
+        }
+
+        #[ink(message)]
+        pub fn call_another_method(&mut self, val: u32) -> u32 {
+            TestExtension.another_method(val)
         }
     }
 }
@@ -66,5 +72,38 @@ mod state_access {
         let mut contract = crate::simple_contract::SimpleContract::new();
         assert_eq!(contract.call_method(100, 200), 1);
         assert_eq!(contract.call_method(100, 200), 2);
+    }
+}
+
+mod internal_calls {
+    #[obce::mock]
+    impl crate::Trait for () {
+        fn method(&mut self, val: u32, another_val: u32) -> u32 {
+            val + another_val
+        }
+
+        fn another_method(&mut self, val: u32) -> u32 {
+            self.method(val, val)
+        }
+    }
+
+    #[test]
+    fn call_contract() {
+        register_chain_extensions(());
+        let mut contract = crate::simple_contract::SimpleContract::new();
+        assert_eq!(contract.call_another_method(200), 400);
+    }
+}
+
+mod unregistered_call {
+    #[obce::mock]
+    impl crate::Trait for () {}
+
+    #[test]
+    #[should_panic(expected = "UnregisteredChainExtension")]
+    fn call_contract() {
+        register_chain_extensions(());
+        let mut contract = crate::simple_contract::SimpleContract::new();
+        contract.call_another_method(200);
     }
 }
